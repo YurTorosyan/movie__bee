f16df2bb8a0fd599253e0f1c5ce481a6ca983694
Yuri <torosyan.yuri17@gmail.com>
2023-05-02 19:18:49 +0400

    Wall aded to website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Header.scss"
 import logo from "../assets/logo.webp"
 
 export default function Header() {
+  const [show, setShow] = useState(false)
+
+  const scrollDetect = () => {
+    if(window.scrollY > 80) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollDetect)
+    return () => window.removeEventListener("scroll", scrollDetect)
+  }, [])
+  
   return (
-    <div className='header'>
+    <div className={`header ${show && "header__shown"}`}>
         <div className="header__container">
           <div className="header__logo">
             <img src={logo} alt="Logo" />
--- src/components/Wall.jsx
+++ src/components/Wall.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "../axios"
 import requests from '../requests';
+import "./Wall.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarWeek, faEarthAmericas, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
+const base_url = 'https://image.tmdb.org/t/p/original';
 
 export default function () {
   const [movie, setMovie] = useState([]);
@@ -18,11 +23,23 @@ export default function () {
   function trancate(str, n) {
     return str?.length > n ? str.slice(0, n - 1) + "..." : str;
   }
-
+  console.log(movie)
   return (
-    <div className='wall'>
+    <div className='wall' style={{
+      backgroundSize: "cover",
+      backgroundImage: `url("${base_url}${movie?.backdrop_path}")`,
+      backgroundPosition: "center"
+    }}>
       <div className="wall__container">
-        
+        <div className="wall__wrap">
+          <h1>{movie?.original_name}</h1>
+          <div className="wall__info">
+            <p><FontAwesomeIcon icon={faStarHalfStroke} /> {movie?.vote_average}</p>
+            <p><FontAwesomeIcon icon={faCalendarWeek} /> {movie?.first_air_date}</p>
+            <p> <FontAwesomeIcon icon={faEarthAmericas} /> {movie?.origin_country}</p>
+          </div>
+          <p>{trancate(movie?.overview, 200)}</p>
+        </div>
       </div>
     </div>
   )
